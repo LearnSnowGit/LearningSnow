17b3ba1545a7dd708547bf610de8de40dce97746
LearnSnowGit <117451559+LearnSnowGit@users.noreply.github.com>
2022-11-04 15:38:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west2.amazonaws.com/dabw/fruit_macros.txt")
 
 #Title
 streamlit.title('Furst tust for me!')
@@ -11,3 +14,6 @@ streamlit.text('This should be text')
 streamlit.text('This should be text \t too')
 streamlit.text('This should be multiline \n text')
 streamlit.text('🥣 🥗 🐔 🥑🍞')
+streamlit.text('\n')
+
+streamlit.dataframe(my_fruit_list)
