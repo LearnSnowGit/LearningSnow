19ce5d24ad46e79221770b042d3d19cb26a495f4
LearnSnowGit <117451559+LearnSnowGit@users.noreply.github.com>
2022-11-04 16:29:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,9 @@ streamlit.text('🥣 🥗 🐔 🥑🍞')
 streamlit.text('\n')
 
 #Interactive
-selected_fruits = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+#First version
 #streamlit.dataframe(my_fruit_list)
-streamlit.dataframe(selected_fruits)
+
+selected_fruits = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
