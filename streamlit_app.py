4662e411c33f2aec95e3818785e80cf8fd79cc61
LearnSnowGit <117451559+LearnSnowGit@users.noreply.github.com>
2022-12-05 14:27:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,8 @@ fruits_to_show = my_fruit_list.loc[selected_fruits]
 streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +"kiwi")
+#streamlit.text(fruityvice_response.json()) # Raw JSON
 
 #JSON looking good
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
