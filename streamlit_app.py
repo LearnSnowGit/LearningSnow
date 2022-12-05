7725dc6796b2dfd48cb1eef448cdf79cd96bf25a
LearnSnowGit <117451559+LearnSnowGit@users.noreply.github.com>
2022-12-05 14:14:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
+#Imports
 import streamlit
 import pandas
+import requests
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
@@ -24,8 +26,9 @@ streamlit.text('\n')
 
 selected_fruits = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[selected_fruits]
-streamlit.dataframe(fruits_to_show)
 
-import requests
+streamlit.dataframe(fruits_to_show)
+streamlit.header("Fruityvice Fruit Advice!")
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
