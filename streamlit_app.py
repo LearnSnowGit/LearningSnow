362267714424fb2cdcfc9a2c5de3b883d19d8da0
LearnSnowGit <117451559+LearnSnowGit@users.noreply.github.com>
2022-12-08 13:28:26 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,5 +58,5 @@ streamlit.text("Fruit list contains:")
 streamlit.dataframe(my_data_rows)
 
 #Adding fruits
-streamlit.write('Thanks for adding ',add_my_fruit)
+streamlit.write('Thanks for adding ',fruit_choice2)
 my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
