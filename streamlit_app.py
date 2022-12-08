f8c8bded82bff7b27ef7af5496e9b06ba080bdcc
LearnSnowGit <117451559+LearnSnowGit@users.noreply.github.com>
2022-12-08 13:42:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,23 +32,34 @@ fruits_to_show = my_fruit_list.loc[selected_fruits]
 streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+#Try block for question
+try:
+  
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  
+  if not fruit_choice:
+     streamlit.error("Please select a fruit")
+  else:
+    streamlit.write('The user entered ', fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+
+except URLError as 3:
+  streamlit.error()
+ 
+  
+fruityvice_response2 = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice2)
+#streamlit.text(fruityvice_response2.json()) # Raw JSON
 
 fruit_choice2 = streamlit.text_input('Second fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice2)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json()) # Raw JSON
-
-fruityvice_response2 = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice2)
-#streamlit.text(fruityvice_response2.json()) # Raw JSON
-
 #JSON looking good
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
 fruityvice_normalized2 = pandas.json_normalize(fruityvice_response2.json())
 #Create table from data
-streamlit.dataframe(fruityvice_normalized)
+
 streamlit.dataframe(fruityvice_normalized2)
 
 streamlit.stop()
