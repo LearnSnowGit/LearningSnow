7995b7a1ebd951dabfdaf49b65accd529dec3b7f
LearnSnowGit <117451559+LearnSnowGit@users.noreply.github.com>
2022-12-08 13:53:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,24 +30,28 @@ selected_fruits = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[selected_fruits]
 
 streamlit.dataframe(fruits_to_show)
-streamlit.header("Fruityvice Fruit Advice!")
 
-#Try block for question
-try:
-  
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  
-  if not fruit_choice:
-     streamlit.error("Please select a fruit")
-  else:
-    streamlit.write('The user entered ', fruit_choice)
+#except URLError as e:
+#  streamlit.error()
+
+#Creating function for fruit pick
+def get_fruityvice_data(this_fruit_choice)  
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    return fruityvice_normalized
 
-except URLError as e:
-  streamlit.error()
- 
+streamlit.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit")
+  else:
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
+
+    
+    
+    
 #Second input box
 fruit_choice2 = streamlit.text_input('Second fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice2)
