b36f94260f460eb98b5be9de24621d45432ada6e
LearnSnowGit <117451559+LearnSnowGit@users.noreply.github.com>
2022-12-08 14:55:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,6 +68,7 @@ def get_fruit_load_list():
 if streamlit.button('Get fruit load list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
+    my_cnx.close()
     streamlit.dataframe(my_data_rows)
     
 def insert_row_snowflake(new_fruit):
@@ -80,6 +81,7 @@ add_my_fruit = streamlit.text_input('What fruit to add?')
 if streamlit.button('Add a fruit to list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
+    my_cnx.close()
     streamlit.text(back_from_function)
     
 
